3127a7517f196e36e64d9a588c13677fe953012b
Yoshimura Yuu <yyu@mental.poker>
2024-06-08 13:14:49 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+The Real World “Referential Transparency”
+============================
+
+[ScalaMatsuri 2024](https://scalamatsuri.org/)の発表スライド
+
+## PDF
+
+- [Slide](https://y-yu.github.io/referential-transparency-slide/referential_transparency.pdf)
+- [Handout](https://y-yu.github.io/referential-transparency-slide/referential_transparency_without_animation.pdf)
---
+++ update_gh-pages.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -eo
+
+TARGET="referential_transparency_without_animation"
+
+if [[ ! $(git rev-parse --verify gh-pages 2>/dev/null) ]]; then
+  git checkout --orphan gh-pages
+  git rm -rf .
+  git clean -fxd
+  git commit --allow-empty -m 'Dummy commit'
+  git checkout master
+fi
+
+make
+
+if [ $? != 0 ]; then
+  make clean
+  make
+fi
+  
+if [ $? = 0 ] && [ -f "${TARGET}.pdf" ] && \
+   [ -f "${TARGET}_without_animation.pdf" ]; then
+  git stash -a
+  git checkout gh-pages
+  rm "${TARGET}.pdf" "${TARGET}_without_animation.pdf" || echo "Fail to delete!"
+  git stash pop
+  git add "${TARGET}.pdf" "${TARGET}_without_animation.pdf"
+  git commit -m 'Update PDFs'
+  git push origin gh-pages
+  git checkout master
+fi
